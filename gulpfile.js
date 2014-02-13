2af5e6b3c51ce91d611efffe8f335d7e5e04a706
Cody Lindley <contact@codylindley.com>
2014-02-13 14:08:13 -0700

    refactor

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var useref = require('gulp-useref');
 var bundle = require('gulp-bundle');
 var runSequence = require('run-sequence');
 var cleanhtml = require('gulp-cleanhtml');
-var imagemin = require('gulp-imagemin');
 var replace = require('gulp-replace');
 var removeLogs = require('gulp-removelogs');
 var size = require('gulp-filesize');
@@ -12,123 +11,146 @@ var htmlhint = require("gulp-htmlhint");
 var csslint = require('gulp-csslint');
 var jshint = require('gulp-jshint');
 var jsonminify = require('gulp-jsonminify');
+var fileinclude = require('gulp-file-include');
+var clean = require('gulp-clean');
+var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
 
 var build = '0.0.0';
 
+//run on src
+
+//replace css and js includes, minify both css and js
+gulp.task('bundleCSSAndHtml', bundle('./src/**/*.html', {
+    appDir: './src',
+    buildDir: './build',
+    minify: false
+}));
+
+//bundle together js and css
+gulp.task('rewriteCSSAndHtmlforBundle', function () {
+    return gulp.src(['./src/**/*.html','!./src/bower/**'])
+        .pipe(useref())
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('includes', function() {
+    return gulp.src(['./build/**/*.html','!./build/bower/**'])
+        .pipe(fileinclude())
+        .pipe(gulp.dest('./build'));
+});
+
+//optimize images
+gulp.task('img', function () {
+    return gulp.src(['./src/**/*.png','./src/**/*.jpg','./src/**/*.gif','./src/**/*.jpeg','./src/**/*.jpg','!./src/bower/**'])
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build'));
+});
+
+
+//run on build
+
+gulp.task('move', function(){
+    // the base option sets the relative root for the set of files,
+    // preserving the folder structure
+    return gulp.src('./build/**/*.*', { base: './build' })
+    .pipe(gulp.dest('production'));
+});
+
+//run on production
+
 gulp.task('htmlHint', function () {
-    gulp.src(['./public/**/*.html','!./public/bower/**'])
+    return gulp.src(['./production/**/*.html','!./production/shared/includes/*.html'])
     .pipe(htmlhint({htmlhintrc:'.htmlhintrc'}))
     .pipe(htmlhint.reporter());
 });
 
 gulp.task('cssLint', function() {
-    gulp.src(['./public/**/*.css','!./public/bower/**'])
+    return gulp.src(['./production/**/*.css','!./production/**/thirdparty.css'])
     .pipe(csslint('.csslintrc'))
     .pipe(csslint.reporter());
 });
 
 gulp.task('validatejsHintRc', function () {
-    gulp.src(['.jshintrcComments'])
+    return gulp.src(['.jshintrcComments'])
     .pipe(jsonminify())
     .pipe(gulp.dest('./'));
 });
 
 gulp.task('jsHint', function() {
-    gulp.src(['./public/**/*.js','!./public/bower/**'])
+    return gulp.src(['./production/**/*.js','!./production/**/thirdparty.js','!./production/**/ie.js'])
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter());
 });
 
-//bundle together js and css
-
-/*
-
-options to minify CSS below:
-
-keepSpecialComments - * for keeping all (default), 1 for keeping first one, 0 for removing all
-keepBreaks - whether to keep line breaks (default is false)
-removeEmpty - whether to remove empty elements (default is false)
-debug - turns on debug mode measuring time spent on cleaning up (run npm run bench to see example)
-root - path with which to resolve absolute @import rules
-relativeTo - path with which to resolve relative @import rules
-
-options to minify JS below
-
-mangle - Pass false to skip mangling names.
-output - Pass an object if you wish to specify additional output options. The defaults are optimized for best compression.
-compress - Pass an object to specify custom compressor options. Pass false to skip compression completely.
-preserveComments - A convenience option for options.output.comments. Defaults to preserving no comments.
-all - Preserve all comments in code blocks
-some - Preserve comments that start with a bang (!) or include a Closure Compiler directive (@preserve, @license, @cc_on)
-function - Specify your own comment preservation function. You will be passed the current node and the current comment and are expected to return either true or false.
-You can also pass the uglify function any of the options listed here(https://github.com/mishoo/UglifyJS2#the-simple-way) to modify UglifyJS's behavior.
-
-*/
-//replace css and js includes, minify both css and js
-gulp.task('bundleCSSAndHtml', bundle('./public/**/*.html', {
-    appDir: './public',
-    buildDir: './publicBuild',
-    minify: true,
-    minifyCss: {},
-    minifyJs: {}
-}));
-
-//bundle together js and css
-gulp.task('rewriteCSSAndHtmlforBundle', function () {
-    return gulp.src(['./public/**/*.html','!./public/bower/**'])
-        .pipe(useref())
-        .pipe(gulp.dest('./publicBuild'));
-});
-
 //minify html
 gulp.task('minifyHtml', function(){
-	return gulp.src(['./publicBuild/**/*.html'])
+    return gulp.src(['./production/**/*.html'])
     .pipe(cleanhtml())
-    .pipe(gulp.dest('./publicBuild'));
-});
-
-//optimize images
-gulp.task('img', function () {
-    gulp.src(['./public/**/*.png','./public/**/*.jpg','./public/**/*.gif','./public/**/*.jpeg','!./public/bower/**'])
-        .pipe(imagemin())
-        .pipe(gulp.dest('./publicBuild'));
+    .pipe(gulp.dest('./production'));
 });
 
 //add cache bust, based on build 
 gulp.task('bustCache', function(){
-    gulp.src(['./publicBuild/**/*.html'])
+    return gulp.src(['./production/**/*.html'])
     .pipe(replace('.css"', '.css?v='+build+'"'))
     .pipe(replace('.js"', '.js?v='+build+'"'))
-    .pipe(gulp.dest('./publicBuild'));
+    .pipe(gulp.dest('./production'));
 });
 
 gulp.task('removeLogs', function(){
-    gulp.src(['./publicBuild/**/*.js'])
+    return gulp.src(['./production/**/*.js'])
     .pipe(removeLogs())
-    .pipe(gulp.dest('./publicBuild'))
+    .pipe(gulp.dest('./production'))
     .pipe(size());
 });
 
-gulp.task('validate', function(callback) {
-    runSequence(
-        'htmlHint',
-        'cssLint',
-        'validatejsHintRc',
-        'jsHint',
-    callback);
+gulp.task('minifyCss', function() {
+    return gulp.src('./production/**/*.css')
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./production'));
+});
+
+gulp.task('minifyJs', function() {
+    return gulp.src('./production/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./production'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch(['./src/**/*.html','./src/**/*.css','./src/**/*.js','!./build/bower/**'],['build']);
 });
 
 gulp.task('build', function(callback) {
 	runSequence(
+        'bundleCSSAndHtml',
+        'rewriteCSSAndHtmlforBundle',
+        'includes',
+        'img',
+    callback);
+});
+
+gulp.task('cleanFiles', function(callback) {
+    gulp.src(['./build','./production'], {read: false})
+    .pipe(clean());
+});
+
+gulp.task('production', function(callback) {
+    runSequence(
+        'bundleCSSAndHtml',
+        'rewriteCSSAndHtmlforBundle',
+        'includes',
+        'img',
+        'move',
+        'bustCache',
         'htmlHint',
         'cssLint',
         'validatejsHintRc',
         'jsHint',
-        'bundleCSSAndHtml',
-        'rewriteCSSAndHtmlforBundle',
         'minifyHtml',
-        'img',
-        'bustCache',
+        'minifyCss',
+        'minifyJs',
         'removeLogs',
     callback);
 });
--- serverBuild.js
+++
@@ -1,5 +0,0 @@
-var connect = require("connect");        
-
-connect().use(connect.static('publicBuild')).listen(3045);
-
-console.log('\nStatic file server running at http://localhost:' + '3045' + '/\n\nCTRL + C to shutdown server');
